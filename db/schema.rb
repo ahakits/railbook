2fce7507ba08987340404755cf2e75fb40f21d99
ahakits <ahakits1987@gmail.com>
2017-10-09 06:04:18 +0900

    Optimistic Locking rails generate scaffold member name:string email:string lock_version:integer add default value to lock_version modify lock_version to hidden_field in form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170920153528) do
+ActiveRecord::Schema.define(version: 20171008205656) do
 
   create_table "authors", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "user_id"
@@ -45,6 +45,14 @@ ActiveRecord::Schema.define(version: 20170920153528) do
     t.string "name"
     t.text "body"
     t.boolean "deleted"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "members", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
+    t.string "email"
+    t.integer "lock_version", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
