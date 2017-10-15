0216e3e65d463a36ba8ef3c4012c826615249462
ahakits <ahakits1987@gmail.com>
2017-10-15 15:48:20 +0900

    in/unless

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   validates :agreement, acceptance: { on: :create }
-  validates :email, confirmation: true
+  validates :email, confirmation: true, presence: { unless: 'dm.blank?' }
 end
