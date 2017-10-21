665cace172edb4b09877c7a9812249e884c7c30d
ahakits <ahakits1987@gmail.com>
2017-10-21 18:36:50 +0900

    after_destroy

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ApplicationRecord
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
   has_and_belongs_to_many :authors
   has_many :users, through: :reviews
   has_many :memos, as: :memoable
@@ -7,6 +7,8 @@ class Book < ApplicationRecord
   scope :gihyo, -> { where(publish: '技術評論社') }
   scope :newer, -> { order(published: :desc) }
   scope :top10, -> { newer.limit(10) }
+
+  after_destroy :history_book
 
 =begin
   validates :isbn,
@@ -36,5 +38,9 @@ class Book < ApplicationRecord
   private
   def isbn_valid?
     errors.add(:isbn, 'は正しい形式ではありません。') unless isbn =~ /\A([0-9]{3}-)?[0-9]-[0-9]{3,5}-[0-9]{4}-[0-9X]\z/
+  end
+
+  def history_book
+    logger.info('delete:' + self.inspect)
   end
 end
